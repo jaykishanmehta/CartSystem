12934f3480a8299926ff696e216e5e717c7363d6
Jaykishan Mehta <jaykishanmehta1991@gmail.com>
2019-12-15 09:26:18 +0530

    Discount helper class updated to make it a working model

--- CartSystem/src/main/java/com/cart/catalog/DiscountHelper.java
+++ CartSystem/src/main/java/com/cart/catalog/DiscountHelper.java
@@ -1,15 +1,29 @@
 package com.cart.catalog;
 
+import java.util.Scanner;
+
+import com.cart.business.DiscountMaster;
+
 public class DiscountHelper {
 
 	public static void main(String[] args) {
 		
 		System.out.println(" -- Discount Helper -- ");
 
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.print("Enter bill amount: ");
+			System.out.println("Final Bill amount: " + getDiscountedPrice(sc.nextFloat()));
+		}
+		
 	}
 
-	public float getDiscountedPrice(float amount) {
+	public static float getDiscountedPrice(float amount) {
 		
+		DiscountMaster discountMaster = new DiscountMaster();
+		
+		amount -= amount* discountMaster.getDiscountForAmount(amount) /100;
 		
 		return amount;
 	}
--- CartSystem/src/test/java/com/cart/catalog/DiscountHelperTest.java
+++ CartSystem/src/test/java/com/cart/catalog/DiscountHelperTest.java
@@ -8,10 +8,9 @@ public class DiscountHelperTest {
 
 	@Test
 	public void test() {
-		DiscountHelper discountHelper = new DiscountHelper();
-		assertTrue("Discounted amount of 5000 should 5000 ", discountHelper.getDiscountedPrice(5000) == 5000);
-		assertTrue("Discounted amount of 10000 should 9500 ", discountHelper.getDiscountedPrice(10000) == 9500);
-		assertTrue("Discounted amount of 15000 should 13500 ", discountHelper.getDiscountedPrice(15000) == 13500);
+		assertTrue("Discounted amount of 5000 should 5000 ", DiscountHelper.getDiscountedPrice(5000) == 5000);
+		assertTrue("Discounted amount of 10000 should 9500 ", DiscountHelper.getDiscountedPrice(10000) == 9500);
+		assertTrue("Discounted amount of 15000 should 13500 ", DiscountHelper.getDiscountedPrice(15000) == 13500);
 	}
 
 }
